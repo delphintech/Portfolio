9c8f6632c84b82df27895d6421a3e85f5c22b3ae
delphine abouab <delphine.abouab@outlook.fr>
2023-11-06 16:29:32 +0100

    custom domain blank

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif', '**/*.scss', '**/*.otf'],
   base: "/Portfolio/"
 })
