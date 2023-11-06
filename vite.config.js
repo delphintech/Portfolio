0f169779d1bac843cdbf384547a7403080d8ca69
delphine abouab <delphine.abouab@outlook.fr>
2023-11-06 15:58:11 +0100

    try

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
-  base: "./Portfolio/"
+  base: "./"
 })
