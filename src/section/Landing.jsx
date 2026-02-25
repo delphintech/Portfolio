dac78c939ce07b86986eab46a2e6898f826d527c
Dabouab <dabouab@student.42.fr>
2026-02-25 17:53:57 +0100

    landing and navbar

--- src/section/Landing.jsx
+++ src/section/Landing.jsx
@@ -1,15 +1,11 @@
-import { Zoom } from "react-awesome-reveal";
-
 export default function Landing() {
   return (
-    <div className="relative flex flex-col flex-wrap justify-center items-center w-full h-full text-back-3 text-shadow-xs text-center z-10 ">
-      <Zoom direction="down" triggerOnce={true} duration={2000} >
+    <div className="animate-blurred-fade-in relative flex flex-col flex-wrap justify-center items-center w-full h-full text-back-3 text-shadow-xs text-center z-10 ">
         <h5 className="subtitle mb-2 text-xl">Bienvenue</h5>
         <h1 className="page-title font-light font-serif text-6xl mb-4">
           <span className=" text-main-1">Delph</span><span className=" text-back-2">in</span>Tech
         </h1>
         <div className="font-bold px-6 py-4 bg-back-2/20 text-red-600 text-base rounded-full drop-shadow-md">Software engineer</div>
-      </Zoom>
     </div>
   )
 }
--- src/section/Navbar.jsx
+++ src/section/Navbar.jsx
@@ -5,127 +5,25 @@ export default function Navbar() {
     const [open, setOpen] = useState(false);
 
     return(
-      <nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0 text-back-3">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <nav className="bg-dark-1/90 fixed w-full z-20 top-0 start-0 text-back-3">
+        <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="#" className="text-xl font-bold">
               <span className="text-main-1">Delph</span><span className="text-back-2">in</span>Tech
             </a>
-          <button onClick={() => setOpen(!open)} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-default" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
+          <button onClick={() => setOpen(!open)} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-md md:hidden hover:bg-back-1/30">
+              <svg className="w-6 h-6"  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
           </button>
-          {
-            open &&
-              <div className="w-full md:block md:w-auto" id="navbar-default">
-                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-white bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0" aria-current="page">Home</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">About</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Services</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Pricing</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Contact</a>
-                  </li>
-                </ul>
-              </div>
-          }
+
+          <div className={`w-full md:block md:w-auto overflow-hidden transition-all duration-300 ${open ? 'max-h-96' : 'max-h-0 md:max-h-none'}`} id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
+              <li><a href="#home" className="block py-2 px-3 rounded hover:bg-back-1/30  p-2">Home</a></li>
+              <li><a href="#about" className="block py-2 px-3 rounded hover:bg-back-1/30  p-2">About</a></li>
+              <li><a href="#resume" className="block py-2 px-3 rounded hover:bg-back-1/30  p-2">Resume</a></li>
+              <li><a href="#portfolio" className="block py-2 px-3  rounded hover:bg-back-1/30 p-2">Projects</a></li>
+            </ul>
           </div>
+        </div>
       </nav>
     )
   }
-    // return (
-    //   <nav className="fixed top-0 w-full bg-dark-1/80 text-back-3 z-50">
-    //     <div className="max-w-7xl mx-auto px-4">
-    //       <div className="flex items-center justify-between h-16">
 
-    //         {/* Logo */}
-    //         <a href="#" className="text-xl font-bold">
-    //           <span className="text-main-1">Delph</span><span className="text-back-2">in</span>Tech
-    //         </a>
-
-    //         {/* Mobile button */}
-    //         <button onClick={() => setOpen(!open)}
-    //           className="lg:hidden text-white focus:outline-none">
-    //           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    //             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} 
-    //               d= {open ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}/>
-    //           </svg>
-    //         </button>
-
-    //         {/* Desktop menu */}
-    //         <ul className="hidden lg:flex space-x-8 ml-auto">
-    //           <li><a href="#home" className="hover:text-red-500">Accueil</a></li>
-    //           <li><a href="#about" className="hover:text-red-500">A propos</a></li>
-    //           <li><a href="#resume" className="hover:text-red-500">Curriculum</a></li>
-    //           <li><a href="#portfolio" className="hover:text-red-500">Portfolio</a></li>
-    //           <li><a href="#contact" className="hover:text-red-500">Contact</a></li>
-    //         </ul>
-
-    //         {/* Mobile menu */}
-    //       {open && (
-    //         <div className="lg:hidden pb-4">
-    //           <ul className="flex flex-col space-y-4">
-    //             <li><a href="#home" className="block hover:text-red-500">Accueil</a></li>
-    //             <li><a href="#about" className="block hover:text-red-500">A propos</a></li>
-    //             <li><a href="#resume" className="block hover:text-red-500">Curriculum</a></li>
-    //             <li><a href="#portfolio" className="block hover:text-red-500">Portfolio</a></li>
-    //             <li><a href="#contact" className="block hover:text-red-500">Contact</a></li>
-    //           </ul>
-    //         </div>
-    //       )}
-
-    //       </div>
-    //     </div>
-    //   </nav>)
-
-    // return (
-    // <nav className="fixed top-0 w-full bg-dark-1/80 text-back-3 z-50">
-    //   <div className="max-w-7xl mx-auto px-4">
-    //     <div className="flex items-center justify-between h-16">
-
-    //       {/* Logo */}
-    //       <a href="#" className="text-xl font-bold">
-    //         <span className="text-main-1">Delph</span><span className="text-back-2">in</span>Tech
-    //       </a>
-
-    //       {/* Mobile button */}
-    //       <button onClick={() => setOpen(!open)}
-    //         className="lg:hidden text-white focus:outline-none">
-    //         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    //           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} 
-    //             d= {open ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}/>
-    //         </svg>
-    //       </button>
-
-    //       {/* Desktop menu */}
-    //       <ul className="hidden lg:flex space-x-8 ml-auto">
-    //         <li><a href="#home" className="hover:text-red-500">Accueil</a></li>
-    //         <li><a href="#about" className="hover:text-red-500">A propos</a></li>
-    //         <li><a href="#resume" className="hover:text-red-500">Curriculum</a></li>
-    //         <li><a href="#portfolio" className="hover:text-red-500">Portfolio</a></li>
-    //         <li><a href="#contact" className="hover:text-red-500">Contact</a></li>
-    //       </ul>
-
-    //       {/* Mobile menu */}
-    //     {open && (
-    //       <div className="lg:hidden pb-4">
-    //         <ul className="flex flex-col space-y-4">
-    //           <li><a href="#home" className="block hover:text-red-500">Accueil</a></li>
-    //           <li><a href="#about" className="block hover:text-red-500">A propos</a></li>
-    //           <li><a href="#resume" className="block hover:text-red-500">Curriculum</a></li>
-    //           <li><a href="#portfolio" className="block hover:text-red-500">Portfolio</a></li>
-    //           <li><a href="#contact" className="block hover:text-red-500">Contact</a></li>
-    //         </ul>
-    //       </div>
-    //     )}
-
-    //     </div>
-    //   </div>
-    // </nav>)
